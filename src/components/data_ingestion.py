41bcf20d4dcbdd442bb75a4916cf206fe9951dfb
SuvitKumar003 <suvitkumar03@gmail.com>
2024-08-01 10:02:57 +0530

    Commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,50 +1,44 @@
 import os
 import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
 from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+from src.utils import save_object, evaluate_models  # Ensure both functions are imported
 
 @dataclass
 class DataIngestionConfig:
-    # Inputs
-    train_data_path: str = os.path.join('artifact', "train.csv")
-    test_data_path: str = os.path.join('artifact', "test.csv")
-    raw_data_path: str = os.path.join('artifact', "data.csv")
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component") 
-
+        logging.info("Entered the data ingestion method or component")
         try:
-            #this line of code is used in reading the data set in case we want to read the data from api or mango db just we need to change the path over here
-            df = pd.read_csv(r'notebook/data/stud.csv')
-            logging.info("Exported the dataset and read it as dataframe")
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info("Train-test split initiated")
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            
+
             logging.info("Ingestion of the data is completed")
 
-            return (
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
-            )
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+
         except Exception as e:
             raise CustomException(e, sys)
-
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
